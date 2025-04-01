aeb443816929f7ea668398cadf688b642e9e94ba
Santi0699 <santisabsol06@gmail.com>
2025-04-01 18:52:13 -0300

    Hasta punto 4

--- Main.c
+++ Main.c
@@ -62,21 +62,57 @@ void print_fibonacci_2(int n)
  //   printf("%d\t",value);
 
 }
+//4
+float pow_recursive(int x, int n)
+{
+
+    if(n==0)
+    {
+        return 1;
+    }
+
+    float result=x;
+
+
+    if(n>0)
+    {
+        
+        return pow_recursive(x,n-1)*result;
+    }
+
+    if(n<0)
+    {
+        return (1.0 / (pow_recursive(x, -(n-1)) * x));
+
+        //return ((1/pow_recursive(x,n+1)) * (1/result));
+        //return (1/(pow_recursive(x,n+1)*x));
+    }
+
+    return result;
+
+}
+
 
 int main ()
 {
 
-    int result=0;
-    result=sum_recursive(4);
-    printf("%d\n",result);
-    result=mult_recursive(2,5);
-    printf("%d\n",result);
+    //int result=0;
+   // result=sum_recursive(4);
+    //printf("%d\n",result);
+    //result=mult_recursive(2,5);
+   // printf("%d\n",result);
 
-    print_fibonacci(4);
+   // print_fibonacci(4);
+
+    //puts(" ");
+
+   // print_fibonacci_2(4);
 
     puts(" ");
 
-    print_fibonacci_2(4);
+    float pepon=0;
+    pepon=pow_recursive(2,-4);
+    printf("%.3f",pepon);
 
 
     return 0;
